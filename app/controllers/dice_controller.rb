d5d584eae790d62911e5582e28331593102ab36b
Victor Kung <victor.y.kung@gmail.com>
2025-08-05 01:40:52 +0000

    Refactored application to use a single dynamic route. Passed tests.

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -3,47 +3,18 @@ class DiceController < ApplicationController
     render({ :template => "dice_templates/homepage" })
   end
 
-  def roll_two_six
+  def roll_flexible
+    @num_dice = params.fetch("number_of_dice").to_i
+    @sides = params.fetch("how_many_sides").to_i
+
     @rolls = []
 
-    2.times do
-      dice = rand(1..6)
+    @num_dice.times do
+      dice = rand(1..@sides)
       @rolls.push(dice)
     end
 
-    render({ :template => "dice_templates/result_two_six" })
+    render({ :template => "dice_templates/result_flexible" })
   end
 
-  def roll_two_ten
-    @rolls = []
-
-    2.times do
-      dice = rand(1..10)
-      @rolls.push(dice)
-    end
-
-    render({ :template => "dice_templates/result_two_ten" })
-  end
-
-  def roll_one_twenty
-    @rolls = []
-
-    1.times do
-      dice = rand(1..20)
-      @rolls.push(dice)
-    end
-
-    render({ :template => "dice_templates/result_one_twenty" })
-  end
-
-  def roll_five_four
-    @rolls = []
-
-    5.times do
-      dice = rand(1..4)
-      @rolls.push(dice)
-    end
-
-    render({ :template => "dice_templates/result_five_four" })
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   get("/", { :controller => "dice", :action => "render_homepage" })
 
-  get("/dice/2/6", { :controller => "dice", :action => "roll_two_six" })
-  get("/dice/2/10", { :controller => "dice", :action => "roll_two_ten" })
-  get("/dice/1/20", { :controller => "dice", :action => "roll_one_twenty" })
-  get("/dice/5/4", { :controller => "dice", :action => "roll_five_four" })
+  get("/dice/:number_of_dice/:how_many_sides", { :controller => "dice", :action => "roll_flexible" })
 end
